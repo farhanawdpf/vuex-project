2e84b36fd1bb6f24ed85ce5d9031277e911f7505
farhanawdpf <farhanawdpf@gmail.com>
2024-05-02 13:06:43 +0600

    vuex

---
+++ vuex/Vuex-Vue-Semaphore-master/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="container">
+    <h1>To-Do Application using Vue.js</h1>
+    <MyTodoForm />
+    <MyTodoList />
+  </div>
+</template>
+<script>
+
+import MyTodoList from './components/MyTodoList';
+import MyTodoForm from './components/MyTodoForm';
+
+export default {
+  name: 'App',
+  components: {
+    MyTodoList,
+    MyTodoForm,
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
